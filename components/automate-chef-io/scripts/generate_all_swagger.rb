942f2b44818e07f710d75e8a70fb646b92528d2b
Pete Higgins <pete@peterhiggins.org>
2019-06-07 11:59:11 -0700

    Tweak combiner script for updated schema.

--- components/automate-chef-io/scripts/generate_all_swagger.rb
+++ components/automate-chef-io/scripts/generate_all_swagger.rb
@@ -3,7 +3,6 @@ require 'json'
 RESULT_FILE_NAME = "all-apis.openapi.json"
 OPENAPI_DIR = File.join(File.dirname(__FILE__), "..", "data/docs/openapi/")
 RESULT_FILE = File.join(OPENAPI_DIR, RESULT_FILE_NAME)
-CONTENT_KEYS = ["definitions", "paths"]
 
 openapi_files = Dir["#{OPENAPI_DIR}/*.json"].reject {|f| File.basename(f) == RESULT_FILE_NAME }
 
@@ -13,9 +12,10 @@ output["info"]["title"] = RESULT_FILE
 openapi_files.each do |openapi_file|
   openapi_json = JSON.parse(File.read(openapi_file))
 
-  CONTENT_KEYS.each do |key|
-    output[key].merge!(openapi_json[key])
-  end
+  output['paths'].merge!(openapi_json['paths'])
+  output['components']['schemas'].merge!(openapi_json['components']['schemas'])
 end
 
 File.write(RESULT_FILE, JSON.dump(output))
+
+puts "Successfully combined files in #{OPENAPI_DIR} into #{RESULT_FILE}."
